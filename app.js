d5fe72bee91e432e5ee584e566ea7725e82a3f35
Cris <c.orav@khk.ee>
2021-12-01 10:16:12 +0200

    added task saving to storage function

--- app.js
+++ app.js
@@ -1,25 +1,75 @@
 // user input form
-const form = document.querySelector('form')
-form.addEventListener('submit', addTask)
+const form = document.querySelector("form");
+form.addEventListener("submit", addTask);
 
-//add task function
+// tasklist
+const tasklist = document.querySelector("ul")
+tasklist.addEventListener("click", delTask)
+
+// delete-button-Link
+const deleteBtn = document.querySelector("#delete-tasks")
+deleteBtn.addEventListener("click", delTasks)
+
+function delTasks(){
+    //tasklist.innerHTML = ""
+    while(tasklist.firstChild) {
+        tasklist.removeChild(tasklist.firstChild)
+    }
+}
+
+//deltask
+function delTask(event){
+    if(event.target.textContent === "X"){
+        if(confirm("Do you want to delete this task?")){
+            event.target.parentElement.remove()
+        }
+    }
+}
+
+
+// add task function
 function addTask(event){
-    //get task value from form input
-    const task = document.querySelector('#task').value
+    // get task value from form input
+    const task = document.querySelector("#task").value
     // get element from DOM
-    const tasklist = document.querySelector('ul');
+    const tasklist = document.querySelector("ul")
     // create element to DOM
-    const li = document.createElement('li')
-    li.className = 'collection-item'
+    const li = document.createElement("li")
+    // add CSS class
+    li.className = "collection-item"
     // add text to element
     const text = document.createTextNode(task)
     li.appendChild(text)
+
+    // create link
+    const link = document.createElement("a")
+    //add css style
+    link.className = "secondary-content"
+    // add text to link
+    link.appendChild(document.createTextNode("X"))
+    // add href attribute
+    link.setAttribute("href", "#")
+    // add link to li
+    li.appendChild(link)
     // add li to tasklist
     tasklist.appendChild(li)
-    // clear form input value
-    document.querySelector('#task').value = ''
 
+    // save task to local storage
+    taskStorage(task)
+
+    //clear form input value
+    document.querySelector("#task").value = " "
     event.preventDefault()
-
+}
+function taskStorage(task){
+    let tasks
+    if(localStorage.getItem("tasks")=== null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"))
+    }
+    tasks.push(task)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
+//console´i: document.querySelector("ul") leiab ul´i dokumendist
